83f766dc4bc24ab6ff150776c5f3c2b01e2d9aab
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2023-08-08 00:44:09 +0300

    frontend update

--- todo-vue/src/stores/user.ts
+++ todo-vue/src/stores/user.ts
@@ -1,23 +1,24 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
-import { type TodoList, type Todo } from '../types'
+import { type Todo, type User } from '../types'
 
 export const useUserStore = defineStore('user', () => {
-  const todoList = ref<TodoList>([
-    {
-      todoId: 0,
-      todoText: '',
-      isDone: false,
-      userId: 0
-    }
-  ])
-  function login(comingTodoList: TodoList) {
+  const userTodos = ref<User>({
+    userId: 0,
+    todoList: []
+  })
+  function login(comingTodoList: User) {
     console.log('comingTodoList', comingTodoList)
-    // comingTodoList.value = comingTodoList
+    userTodos.value = comingTodoList
+  }
+  function register(comingUserId: number) {
+    console.log('comingUserId', comingUserId)
+    userTodos.value.userId = comingUserId
+    userTodos.value.todoList = []
   }
   function addTodo(comingTodo: Todo) {
-    todoList.value.push(comingTodo)
+    userTodos.value.todoList.push(comingTodo)
   }
 
-  return { todoList, addTodo, login }
+  return { userTodos, addTodo, login, register }
 })
