4293817048e16919b9bcd7fc1ef6d0e5958be8e5
Oğulcan Munoğulları <89843337+ogulcanmunogullari@users.noreply.github.com>
2023-08-09 00:37:51 +0300

    frontend update

--- todo-vue/src/stores/user.ts
+++ todo-vue/src/stores/user.ts
@@ -1,11 +1,25 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { type Todo, type User } from '../types'
+import axios from 'axios'
 
 export const useUserStore = defineStore('user', () => {
   const userTodos = ref<User>({
     userId: 0,
-    todoList: []
+    todoList: [
+      {
+        todoId: 0,
+        todoText: 'test',
+        isDone: false,
+        userId: 0
+      },
+      {
+        todoId: 1,
+        todoText: 'test2',
+        isDone: true,
+        userId: 0
+      }
+    ]
   })
   function login(comingTodoList: User) {
     console.log('comingTodoList', comingTodoList)
@@ -16,9 +30,39 @@ export const useUserStore = defineStore('user', () => {
     userTodos.value.userId = comingUserId
     userTodos.value.todoList = []
   }
-  function addTodo(comingTodo: Todo) {
-    userTodos.value.todoList.push(comingTodo)
+  async function addTodo(comingTodoText: string) {
+    const response = await axios.post('http://localhost:5292/api/Todo/id', {
+      text: comingTodoText,
+      id: userTodos.value.userId
+    })
+    if (response.status == 200) {
+      const comingTodo: Todo = { ...response.data }
+      userTodos.value.todoList.push(comingTodo)
+    } else if (response.status == 404) {
+      return 'User not found'
+    }
+  }
+  async function toggleTodo(comingTodoId: number) {
+    const todo = userTodos.value.todoList.find((todo) => todo.todoId == comingTodoId)
+    if (todo) {
+      const response = await axios.put('http://localhost:5292/api/Todo', {
+        _boolean: !todo.isDone,
+        todoId: todo.todoId
+      })
+      if (response.status == 200) {
+        todo.isDone = !todo.isDone
+      }
+    }
   }
 
-  return { userTodos, addTodo, login, register }
+  async function removeTodo(comingTodoId: number) {
+    const response = await axios.delete(`http://localhost:5292/api/Todo/${comingTodoId}`)
+    if (response.status == 200) {
+      userTodos.value.todoList = userTodos.value.todoList.filter(
+        (todo) => todo.todoId != comingTodoId
+      )
+    }
+  }
+
+  return { userTodos, addTodo, login, register, toggleTodo, removeTodo }
 })
--- todo-vue/src/views/TodosView.vue
+++ todo-vue/src/views/TodosView.vue
@@ -1,19 +1,86 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useUserStore } from '@/stores/user'
+import { ref } from 'vue'
+
+const userStore = useUserStore()
+const filter = ref<string>('all')
+const todoText = ref<string>('')
+const filterEnum: {
+  [key: string]: string | boolean
+} = {
+  all: '',
+  active: true,
+  completed: false
+}
+const changeFilter = (comingFilter: string) => {
+  filter.value = comingFilter
+}
+const addTodoFn = () => {
+  const err = userStore.addTodo(todoText.value)
+  if (err) {
+    alert(err)
+  }
+  todoText.value = ''
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="mainDiv">
+    <div>
+      <input type="text" v-model="todoText" v-on:keyup.enter="addTodoFn" />
+    </div>
+    <ul
+      :key="todo.todoId"
+      v-for="todo in userStore.userTodos.todoList.filter(
+        (todo) => todo.isDone !== filterEnum[filter]
+      )"
+    >
+      <li>
+        <p :class="`${todo.isDone == true && 'isDone'}`">
+          {{ todo.todoText }}
+        </p>
+        <div class="buttonGroup">
+          <button @click="() => userStore.toggleTodo(todo.todoId)">
+            {{ todo.isDone == true ? 'UnDone' : 'Done' }}
+          </button>
+          <button @click="() => userStore.removeTodo(todo.todoId)">Remove</button>
+        </div>
+      </li>
+    </ul>
+    <div>
+      <button @click="() => changeFilter('all')">All</button>
+      <button @click="() => changeFilter('active')">Active</button>
+      <button @click="() => changeFilter('completed')">Completed</button>
+    </div>
+  </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.mainDiv {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  width: 500px;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  width: 100%;
+}
+li {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: rgb(199, 16, 16);
+}
+.isDone {
+  text-decoration: line-through;
+}
+.buttonGroup {
+  display: flex;
+  gap: 5px;
+  text-decoration: unset;
 }
 </style>
